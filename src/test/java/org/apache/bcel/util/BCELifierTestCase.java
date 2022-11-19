c64245ec2f3d2d7f571e2f877083771c4ebc2c94
nbauma109 <nbauma109@github.com>
2022-11-19 17:42:07 +0100

    added org.apache.commons.lang.math.Fraction.class

--- src/test/java/org/apache/bcel/util/BCELifierTestCase.java
+++ src/test/java/org/apache/bcel/util/BCELifierTestCase.java
@@ -103,6 +103,7 @@ public class BCELifierTestCase extends AbstractTestCase {
     @ParameterizedTest
     @ValueSource(strings = {
     // @formatter:off
+        "org.apache.commons.lang.math.Fraction.class",
         "org.apache.commons.lang.builder.CompareToBuilder.class",
         "org.apache.commons.lang.builder.ToStringBuilder.class",
         "org.apache.commons.lang.SerializationUtils.class",
