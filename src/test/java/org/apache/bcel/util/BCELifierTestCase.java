e415e6b5d064cc46b78418b19681d9558dbd2671
nbauma109 <nbauma109@github.com>
2022-11-19 18:23:46 +0100

    NestableDelegate

--- src/test/java/org/apache/bcel/util/BCELifierTestCase.java
+++ src/test/java/org/apache/bcel/util/BCELifierTestCase.java
@@ -105,8 +105,8 @@ public class BCELifierTestCase extends AbstractTestCase {
     @ValueSource(strings = {
     // @formatter:off
         "org.apache.commons.lang.math.Fraction.class",
+        "org.apache.commons.lang.exception.NestableDelegate.class",
         "org.apache.commons.lang.builder.CompareToBuilder.class",
-        "org.apache.commons.lang.builder.ReflectionToStringBuilder.class",
         "org.apache.commons.lang.builder.ToStringBuilder.class",
         "org.apache.commons.lang.SerializationUtils.class",
         "org.apache.commons.lang.ArrayUtils.class",
