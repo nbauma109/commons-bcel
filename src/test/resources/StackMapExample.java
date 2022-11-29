2d0adfad8813e9919cbc86be53f2b5edc2499224
nbauma109 <nbauma109@github.com>
2022-11-29 07:14:40 +0100

    added some abstract method to StackMapExample

--- src/test/resources/StackMapExample.java
+++ src/test/resources/StackMapExample.java
@@ -2,7 +2,9 @@
  * Example to check if BCELifier generates the stack map table
  * so that the generated class passes the Java verifier checks.
  */
-public class StackMapExample {
+public abstract class StackMapExample {
+
+    protected abstract void someAbstractMethod();
 
 	public static void main(final String[] args) {
 		switch (args[0]) {
