01e1d2827bf7ee4045e79785016d99466b8fa545
nbauma109 <nbauma109@users.noreply.github.com>
2022-11-29 09:49:30 +0100

    Fix line length

--- src/main/java/org/apache/bcel/classfile/StackMapType.java
+++ src/main/java/org/apache/bcel/classfile/StackMapType.java
@@ -31,7 +31,7 @@ import org.apache.bcel.Const;
  */
 public final class StackMapType implements Node, Cloneable {
 
-    static final StackMapType[] EMPTY_ARRAY = {}; // package visibility because BCELifier code generator writes calls to constructor which translates null to EMPTY_ARRAY
+    static final StackMapType[] EMPTY_ARRAY = {}; // package visibility as BCELifier code generator writes calls to constructor translating null to EMPTY_ARRAY
 
     private byte type;
     private int index = -1; // Index to CONSTANT_Class or offset
